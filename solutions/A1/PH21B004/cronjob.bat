e15eee9e6fdc5698a5c969ef4b9544124d1919e5
BullsEye268 <100084519+BullsEye268@users.noreply.github.com>
2025-02-26 14:03:49 +0530

    Add files via upload

---
+++ solutions/A1/PH21B004/cronjob.bat
@@ -0,0 +1,6 @@
+@echo off
+REM Navigate to the script's directory (replace with your path)
+cd /d "%~dp0"
+
+REM Run the script using the Conda environment's Python executable (replace paths)
+"C:/Users/DELL/.conda/envs/MLOps/python.exe" "Modules/module6.py"
