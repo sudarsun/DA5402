c22a450496129de01ade4c01b2395c3f66995cf6
Dr. Su Sa <sudarsun@gmail.com>
2025-03-16 17:34:00 +0530

    added notes to functionalities. added logging added additional metrics

--- examples/monitoring/ai_app.py
+++ examples/monitoring/ai_app.py
@@ -9,46 +9,85 @@ from fastapi import FastAPI, Body, Request
 from pydantic import BaseModel
 import spacy
 import uvicorn
-import time
+import logging
 
-from prometheus_client import Summary, start_http_server, Counter, Gauge
-#from prometheus_client import disable_created_metrics
+from prometheus_client import Summary, start_http_server, Counter, Gauge, Info
+from prometheus_client import disable_created_metrics
 
-# disable _created metric.
-#disable_created_metrics()
+# disable **_created metric.
+disable_created_metrics()
 
-REQUEST_DURATION = Summary('api_timing', 'Request duration in seconds')
+request_size = Summary('request_size_bytes', 'input data size (bytes)')  # _sum will track the total bytes, _count will track the number of calls.
+api_usage = Summary('api_usage', 'api run time monitoring') # _sum tracks total time taken, _count tracks number of calls.
+
+# define the counter to track the usage based on client IP.
 counter = Counter('api_call_counter', 'number of times that API is called', ['endpoint', 'client'])
+
 gauge = Gauge('api_runtime_secs', 'runtime of the method in seconds', ['endpoint', 'client']) 
+
+info = Info('build_info', 'Prometheus Instrumented AI App')
+info.info({'version': '0.0.13', 'buildhost': 'drsudar@resonance'})
+
 nlp_en = spacy.load("en_core_web_sm")
 app = FastAPI(title="First AI application")
+
+log = logging.getLogger("AI_app")
+log.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+ch.setFormatter(formatter)
+log.addHandler(ch)
 
 class Data(BaseModel):
     text:str
 
-@REQUEST_DURATION.time()
+# NOTE: If the order of the decorator is swapped, api_usage does not work.
 @app.post("/np")
+@api_usage.time()
 def extract_np(data:Data, lang:str, request:Request):
+    log.info(f"extract_np: received {len(data.text)} bytes on input")
+    
+    # let's track the amount of data processed by this API.
+    request_size.observe(amount=len(data.text))
+    # increment the counter per host per api.
     counter.labels(endpoint='/np', client=request.client.host).inc()
 
-    start = time.time()
     doc_en = nlp_en(data.text)
     nps = [ch for ch in map(lambda x: x.text, doc_en.noun_chunks)]
-    time_taken = time.time() - start
 
-    gauge.labels(endpoint='/np', client=request.client.host).set(time_taken)
+    # report the running time of the api.
+    #gauge.labels(endpoint='/np', client=request.client.host).set(time_taken)
 
     return {"input":data.text, "NP":nps, "lang":lang}
 
 @app.post('/ne')
 def extract_ne(data:Data, request:Request):
+    log.info(f"extract_ne: received {len(data.text)} bytes on input")
+
+    # let's track the amount of data processed by this API.
+    request_size.observe(amount=len(data.text))
+    # increment the counter per host per api.
     counter.labels(endpoint='/ne', client=request.client.host).inc()
-    doc_en = nlp_en(data.text)
-    ne = dict(map(lambda x: (x.text,x.label_), doc_en.ents))
+
+    with api_usage.time():
+        doc_en = nlp_en(data.text)
+        ne = dict(map(lambda x: (x.text,x.label_), doc_en.ents))
+
+    # report the running time of the api.
+    #gauge.labels(endpoint='/np', client=request.client.host).set(time_taken)
+
     return {"input":data.text, "NE":ne}
 
+# NOTE: If the order of the decorator is swapped, api_usage does not work.
 @app.post("/nptext")
+@api_usage.time()
 async def extract_body(text:str=Body(...), request:Request=None):
+    log.info(f"extract_body: received {len(text)} bytes on input")
+
+    # let's track the amount of data processed by this API.
+    request_size.observe(amount=len(text))
+    # increment the counter per host per api.
     counter.labels(endpoint='/nptext', client=request.client.host).inc()
     lines = text.split("\n")
     records = []
@@ -60,6 +99,7 @@ async def extract_body(text:str=Body(...), request:Request=None):
     return {"results": records}
 
 # start the exporter metrics service
+log.info("starting the prometheus monitor at port 18000")
 start_http_server(18000)
 
 # Run from command line: uvicorn ai_app:app --port 7000 --host 0.0.0.0
